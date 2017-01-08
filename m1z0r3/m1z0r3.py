83a8f34f5b8536a587d19acaf66fbfbde7ccae04
Ryo <ryoryongaryo5679@gmail.com>
2017-01-08 20:18:47 +0900

    add rsa, crt

--- m1z0r3/m1z0r3.py
+++ m1z0r3/m1z0r3.py
@@ -43,6 +43,27 @@ def modInv(a, m):
  else:
      return x % m
 
+def chinese_remainder(n, a):
+	sum = 0
+	prod = reduce(lambda a, b: a*b, n)
+		 
+	for n_i, a_i in zip(n, a):
+		p = prod / n_i
+		sum += a_i * mul_inv(p, n_i) * p
+	return sum % prod
+
+def mul_inv(a, b):
+  b0 = b
+  x0, x1 = 0, 1
+  if b == 1: return 1
+  while a > 1:
+    q = a / b
+    a, b = b, a%b
+    x0, x1 = x1 - q * x0, x0
+  if x1 < 0: 
+    x1 += b0
+  return x1
+
 def continued_fractions(n,e):
   cf = [0]
   while e != 0:
@@ -84,6 +105,17 @@ def calcPQ(a,b):
 #============
 # RSA cipher
 #============
+def rsa(p,q,e=65537):
+  """
+  RSA Cipher
+  @param  p int: prime number1
+  @param  q int: prime number2
+  @param  e int: public exponent
+  @return d int: secret key
+  """
+  phi = (p-1) * (q-1)
+  return modInv(e,phi)
+
 def common(c, e, n):
   """
   Common Modulus Attack
